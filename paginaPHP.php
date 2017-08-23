a3dfde1667eb7b5d9242fbc0440ef17f0cd8004f
jose aldair nava correa <aldair_zaragoza@hotmail.com>
2017-08-22 20:43:24 -0500

    PRACTICA DEL DIA MARTES 22 DE AGOSTO DEL 2017

---
+++ paginaPHP.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//ES">
+<HTML>
+<HEAD>
+<meta charset="UTF-8">
+      
+       <title>MI PRIMER PAGINA PHP</title>
+       
+ <TITLE>Jose Aldair Nava Correa</TITLE>
+</HEAD>
+<BODY>
+<h1>Mi pagina PHP</h1>
+<?php
+$variable1 = 1;
+echo "jose aldair nava correa <br>";
+echo "ingenieria computacional <br>";
+echo "el dia del eclipse no se vio nada <br>";
+echo "byll Nyde e cientifico transmitio en vivo <br>";
+
+
+?>
+</BODY>
+</HTML>
